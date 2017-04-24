e85da6663355ea34b4e2f9cf405c7763e018fdf0
Martin Malinvaud <martin.malinvaud@gmail.com>
2017-04-24 19:59:46 +0200

    controller view show index update delete

--- app/controllers/culinarystyles_controller.rb
+++ app/controllers/culinarystyles_controller.rb
@@ -1,12 +1,51 @@
 class CulinarystylesController < ApplicationController
+  before_action :set_culinarystyle, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show, :new, :edit, :update, :create]
 
-  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def new
+    @culinarystyle = Culinarystyle.new
+  end
+
+  def edit
+  end
 
   def index
     @culinarystyles = Culinarystyle.all
   end
 
+  def show
+  end
+
+  def create
+    @culinarystyle = Culinarystyle.new(culinarystyle_params)
+    @culinarystyle.save
+
+    redirect_to culinarystyle_path(@culinarystyle)
+  end
+
+  def update
+    @culinarystyle.update(culinarystyle_params)
+
+    redirect_to culinarystyle_path(@culinarystyle)
+
+  end
+
   def destroy
     @culinarystyle.destroy
+
+    redirect_to culinarystyles_path
+  end
+
+  private
+
+  def set_culinarystyle
+    @culinarystyle = Culinarystyle.find(params[:id])
+  end
+
+  def culinarystyle_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:culinarystyle).permit(:name, :description)
   end
 end
